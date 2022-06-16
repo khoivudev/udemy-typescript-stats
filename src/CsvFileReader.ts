710569a1d99bfdc415ad56646cca84df5d13609c
khoi.vu <khoi.vu@access-company.com>
2022-06-16 15:25:21 +0900

    [refactor] Implement csv file reader

---
+++ src/CsvFileReader.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+export abstract class CsvFileReader {
+  data: MatchData[] = [];
+
+  constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): MatchData;
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: 'utf-8',
+      })
+      .split('\n')
+      .map((row: string): string[] => {
+        return row.split(',');
+      })
+      .map(this.mapRow);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
-import fs from 'fs';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new MatchReader('football.csv');
+reader.read();
 
-enum MatchResult {
-  homeWin = 'H',
-  awayWin = 'A',
-  draw = 'D',
-}
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] == 'Man United' && match[5] === MatchResult.homeWin) {
     manUnitedWins++;
   } else if (match[2] == 'Man United' && match[5] === MatchResult.awayWin) {
